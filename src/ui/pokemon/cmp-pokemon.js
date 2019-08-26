826a5d7f8cc266d28221b5f73656d945972d7af9
rchow1944 <rchow2@buffalo.edu>
2019-08-26 19:29:08 -0400

    Converted to functional component

--- src/ui/pokemon/cmp-pokemon.js
+++ src/ui/pokemon/cmp-pokemon.js
@@ -1,24 +1,20 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 
-class Pokemon extends PureComponent {
-  render() {
-    const { pokemon } = this.props
-
-    return (
-      <div className="pokemon">
-        <button
-          type="button"
-          className="pokemon__sprite"
-          style={{
-            backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
-              pokemon.id
-            }.png`})`
-          }}
-        />
-        <p className="pokemon__name">{pokemon.name}</p>
-      </div>
-    )
-  }
+const Pokemon = ({ pokemon }) => {
+  return (
+    <div className="pokemon">
+      <button
+        type="button"
+        className="pokemon__sprite"
+        style={{
+          backgroundImage: `url(${`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+            pokemon.id
+          }.png`})`
+        }}
+      />
+      <p className="pokemon__name">{pokemon.name}</p>
+    </div>
+  )
 }
 
 export default Pokemon
